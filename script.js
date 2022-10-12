67c04689f3e6b3e553d2561a9162bf5bcbf73a85
Francisco Cavalcanti <franciscoreis85@hotmail.com>
2022-10-12 19:05:11 -0300

    Insert/Paste function finisihed

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// [[ Copy tooltip text]]
+// [[ Copy & Paste tooltip text]]
 
 let tag = document.getElementById("tagNav").getElementsByClassName("tooltip");
+const inputTag = document.getElementById("cmd-k-input"); // Id has to be same as displayed on Intercom
 
 for (let i=0; i < tag.length; i++) {
   tag[i].onclick = function () {
     let tagSelect = tag[i].dataset.text;
-    navigator.clipboard.writeText(tagSelect);
+    navigator.clipboard.writeText(tagSelect); // Copies tooltip text
+    inputTag.value = tagSelect; // Inserts copied text into input field (as used by Intercom on line 4)
   };
 }
