827fe726617f491f1542fc9c7f0aa1ca8f6a2d07
Francisco Cavalcanti <franciscoreis85@hotmail.com>
2022-10-13 10:12:42 -0300

    Tooltip visibility

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// [[ Open ]]
+
+// Array.from(document.getElementsByClassName("conatiner-home"))
+//   .forEach((element) => element.addEventListener("click", function() {
+//     alert("Hello World!")
+//   }
+//   ))
+
 // [[ Copy & Paste tooltip text]]
 
 let tag = document.getElementById("tagNav").getElementsByClassName("tooltip");
@@ -9,4 +17,17 @@ for (let i=0; i < tag.length; i++) {
     navigator.clipboard.writeText(tagSelect); // Copies tooltip text
     inputTag.value = tagSelect; // Inserts copied text into input field (as used by Intercom on line 4)
   };
+}
+
+
+// [[ Visibility ]]
+
+let visibleTags = document.getElementById("tagNav").getElementsByClassName("tagNavArea");
+let whiteTags = document.getElementById("tagTableMain");
+
+for (let i=0; i < visibleTags.length; i++) {
+  visibleTags[i].onclick = function () {
+    whiteTags.classList.toggle("fade");
+
+  }
 }
