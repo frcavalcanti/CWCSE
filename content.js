6df78e441ee182211c72b2d8fc4505cc1ed4dbbf
Francisco Cavalcanti <franciscoreis85@hotmail.com>
2022-10-19 13:41:58 -0300

    Testing trigger

--- content.js
+++ content.js
@@ -1,7 +1,18 @@
+console.log("Toolbox Content Script Loaded")
+const cmdInput = document.getElementById("cmd-k-input")
+let ev = new KeyboardEvent('keydown', {
+  key: 'a',
+});
+
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
       try {
-          document.getElementById("cmd-k-input").value = request.tooltip;
+          cmdInput.value = request.tooltip;
+          setTimeout(() => {
+            cmdInput.dispatchEvent(ev);
+            cmdInput.value += " !"
+          }, 1000);
           sendResponse({status: "Success!"});
       } catch (error) {
           console.log(error)
