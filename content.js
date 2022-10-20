c441169702ef64d1e12d0bc3dc08a5fbc0d962d2
Francisco Cavalcanti <102861632+frcavalcanti@users.noreply.github.com>
2022-10-19 22:50:56 -0300

    Revert "Testing trigger"

--- content.js
+++ content.js
@@ -1,18 +1,7 @@
-console.log("Toolbox Content Script Loaded")
-const cmdInput = document.getElementById("cmd-k-input")
-let ev = new KeyboardEvent('keydown', {
-  key: 'a',
-});
-
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
       try {
-          cmdInput.value = request.tooltip;
-          setTimeout(() => {
-            cmdInput.dispatchEvent(ev);
-            cmdInput.value += " !"
-          }, 1000);
+          document.getElementById("cmd-k-input").value = request.tooltip;
           sendResponse({status: "Success!"});
       } catch (error) {
           console.log(error)
